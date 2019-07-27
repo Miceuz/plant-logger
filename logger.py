0fb40ba7bef10a4fb35b19715ae253f3ffce1ae1
miceuz <mic@hardcore.lt>
2019-07-27 19:45:40 +0000

    Add a field indicating sensor outage, add a LED

--- logger.py
+++ logger.py
@@ -3,6 +3,9 @@ import minimalmodbus
 import serial
 from time import sleep, time
 from influxdb import InfluxDBClient
+import RPi.GPIO as gpio
+gpio.setmode(gpio.BCM)
+gpio.setup(27, gpio.OUT)
 
 
 SENSOR_PORT = '/dev/ttyUSB0'
@@ -23,19 +26,36 @@ dbclient = InfluxDBClient(host='localhost', port=8086)
 dbclient.switch_database('soil_sensors')
 
 while True:
-	if time() - lastPostTimestamp > 30:
+	if time() - lastPostTimestamp > 3:
 		try:
 			moisture = sensor.read_register(0, functioncode=4)
 			temperature = sensor.read_register(1, functioncode=4, numberOfDecimals=1, signed=True)
 			point=[
 			{"measurement":"data", 
-			"tags":{"plant":"begonija"}, 
+                            "tags":{"plant":"begonija"}, 
 			"fields":{"temperature":temperature*1.0, "moisture":moisture*1.0}}]
+			#print(point)				
 			try:
 				dbclient.write_points(point)
+				gpio.output(27, gpio.HIGH)
 			except:
 				print("#could not post")
+				gpio.output(27, gpio.LOW)
+
 		except(IOError, ValueError):
-			print("#could not read sensor")
+                        point=[
+                             {"measurement":"data",
+                            "tags":{"plant":"begonija"},
+                            "fields":{"sensor_status":"pyzdiec"}
+                            }] 
+                        			
+                             
+			try:
+				dbclient.write_points(point)
+				gpio.output(27, gpio.HIGH)
+			except:
+				print("#could not post")
+				gpio.output(27, gpio.LOW)
+                        print("#could not read sensor")
 		
 		lastPostTimestamp = time()
